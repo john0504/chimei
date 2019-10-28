8b424c1fa5f6e4a202aedecc7d4a5c83d7470e65
john0504 <john2000@mail2000.com.tw>
2019-10-28 13:45:00 +0800

    first commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,91 @@
+export interface AppConfig {
+  appEngine: {
+    solutionId: string,
+    productId: string,
+  };
+  mixpanel: {
+    token: string,
+  };
+  hockeyApp: {
+    ids: {
+      android: string,
+      ios: string,
+    }
+  };
+  app: {
+    group: {
+      max: number,
+      devices: number,
+    },
+    schedule: {
+      max: number,
+    },
+    support: {
+      email: string,
+    },
+    disableLog?: boolean;
+    logConfig: {
+      recordLimit?: number,
+    }
+    theme: {
+      primaryColor: string,
+      productName: string,
+      wifiName: string,
+    },
+    amazonAlexa: {
+      skillName: string,
+    },
+    ifttt: {
+      actionName: string,
+    },
+    googleHome: {
+      ghName: string,
+    },
+  };
+}
+
+export const appConfig: AppConfig = {
+  appEngine: {
+    solutionId: 'hamv-anfson',
+    productId: 'ws1lqtb0lyk00000',
+  },
+  mixpanel: {
+    token: 'YOUR_TOKEN',
+  },
+  hockeyApp: {
+    ids: {
+      android: '',
+      ios: '',
+    }
+  },
+  app: {
+    group: {
+      max: 10, // Max group per user
+      devices: 10, //Max device number per group
+    },
+    schedule: {
+      max: 10, // Max schedule number per device
+    },
+    support: {
+      email: '',
+    },
+    disableLog: false,
+    logConfig: {
+      recordLimit: 10000,
+    },
+    theme: {
+      primaryColor: '#00baff',
+      productName: 'CHIMEI 智慧家電',
+      wifiName: 'CHIMEI-XXXX',
+    },
+    amazonAlexa: {
+      skillName: 'skill-name',
+    },
+    ifttt: {
+      actionName: 'ifttt_name',
+    },
+    googleHome: {
+      ghName: 'gh_name',
+    },
+  }
+};
